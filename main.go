6b07c75103939d349fb038bf0162ee37620cb9e9
bokwoon <bokwoon.c@gmail.com>
2023-04-01 18:58:36 +0800

    checkpoint

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"golang.org/x/crypto/acme/autocert"
 )
@@ -13,11 +15,15 @@ import (
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	homeDir := "/"
+	if h := os.Getenv("HOME"); h != "" {
+		homeDir = h
+	}
 	certManager := autocert.Manager{
 		Prompt: autocert.AcceptTOS,
-		Cache:  autocert.DirCache("certs"),
+		Cache:  autocert.DirCache(filepath.Join(homeDir, ".cache", "golang-autocert")),
 		HostPolicy: func(ctx context.Context, host string) error {
-			log.Println(host)
+			log.Println("HostPolicy:", host)
 			return nil
 		},
 	}
